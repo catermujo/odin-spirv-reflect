974e30aab3f753244f5e8746ebe711dd1638e081
Isabella Basso <isabellabdoamaral@gmail.com>
2026-04-16 20:21:18 -0300

    fix windows spirv build output and ignore local build tree

--- build.bat
+++ build.bat
@@ -3,17 +3,29 @@
 setlocal EnableDelayedExpansion
 
 if not exist SPIRV-Reflect (
-    git clone --recurse-submodules --revision ef913b3ab3da1becca3cf46b15a10667c67bebe5 https://github.com/KhronosGroup/SPIRV-Reflect --depth=1
+    git clone --recurse-submodules --revision ef913b3ab3da1becca3cf46b15a10667c67bebe5 https://github.com/KhronosGroup/SPIRV-Reflect --depth=1 || exit /b 1
 )
 
+set source_dir=SPIRV-Reflect
 set binaries_dir=build
 
 echo Configuring build...
-cmake . -B %binaries_dir% -DSPIRV_REFLECT_EXECUTABLE=OFF -DSPIRV_REFLECT_STATIC_LIB=ON -DCMAKE_BUILD_TYPE=Release
+REM DUMBAI: Configure the checked-out upstream project instead of the wrapper folder.
+cmake -S %source_dir% -B %binaries_dir% -A x64 -DSPIRV_REFLECT_EXECUTABLE=OFF -DSPIRV_REFLECT_STATIC_LIB=ON -DCMAKE_BUILD_TYPE=Release || exit /b 1
 
 echo Building project...
-make -C %binaries_dir%
+REM DUMBAI: Let CMake pick the active Windows generator instead of assuming make exists.
+cmake --build %binaries_dir% --config Release || exit /b 1
 
-copy /y %binaries_dir%\spirv-reflect-static.lib .\spirv.lib
+set SPIRV_LIB=
+for /r "%binaries_dir%" %%F in (*.lib) do (
+    if /I "%%~nxF"=="spirv-reflect-static.lib" set "SPIRV_LIB=%%~fF"
+)
+if not defined SPIRV_LIB (
+    echo ERROR: Could not find spirv-reflect-static.lib under %binaries_dir%.
+    exit /b 1
+)
+
+copy /y "%SPIRV_LIB%" spirv.lib >nul || exit /b 1
 
 echo Build completed successfully!
