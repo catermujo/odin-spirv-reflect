bd99dd2abf0c8392e3242ee04e8498ca7d00fa3a
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-27 19:44:49 -0300

    build: replace git clone --revision with checkout fallback

--- build.sh
+++ build.sh
@@ -2,7 +2,23 @@
 
 set -e
 
-[ -d SPIRV-Reflect ] || git clone --revision ef913b3ab3da1becca3cf46b15a10667c67bebe5 https://github.com/KhronosGroup/SPIRV-Reflect --depth 1
+clone_at_revision() {
+    local dir="$1"
+    local revision="$2"
+    local remote="$3"
+    shift 3
+    [ -d "$dir" ] && return
+    git clone "$@" "$remote" "$dir"
+    if ! git -C "$dir" checkout --detach "$revision"; then
+        git -C "$dir" fetch origin "$revision"
+        git -C "$dir" checkout --detach FETCH_HEAD
+    fi
+    if [ -f "$dir/.gitmodules" ]; then
+        git -C "$dir" submodule update --init --recursive
+    fi
+}
+
+clone_at_revision SPIRV-Reflect ef913b3ab3da1becca3cf46b15a10667c67bebe5 https://github.com/KhronosGroup/SPIRV-Reflect --depth=1
 
 echo "Building SPIRV-Reflect.."
 cd SPIRV-Reflect
